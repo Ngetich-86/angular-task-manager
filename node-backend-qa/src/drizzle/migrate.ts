0db4037de0ff4c4498f6422410218d36912d5e3d
Ngetich-86 <gideonkngetich86@gmail.com>
2025-07-12 17:08:13 +0300

    feat: db set up and drizzle orm set up

---
+++ node-backend-qa/src/drizzle/migrate.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+
+import { pool } from "./db";
+import db from "./db";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+async function migration() {
+
+    console.log("======== Migrations started ========")
+    await migrate(db, { migrationsFolder: __dirname + "/migrations" })
+    await pool.end()
+    console.log("======== Migrations ended ========")
+    process.exit(0)
+
+}
+
+migration().catch((err) => {
+    console.error(err)
+    process.exit(1)
+})
